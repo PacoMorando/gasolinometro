fdbde9b9e55cf83e4011bd0337f6c25af052ffa9
NYP_PC <paco.morando@gmail.com>
2023-10-29 21:00:01 -0600

    adding data binding

--- app/src/main/java/sas/gasolinometro/MainActivity.java
+++ app/src/main/java/sas/gasolinometro/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import sas.gasolinometro.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    public String textMessage = "testBinding TEXTO PARA VER SI JALA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //this.textMessage = getResources().getDisplayMetrics().toString() + getResources() + " DATA BINDING";
+        binding.textDef.setText(getResources().getDisplayMetrics().toString() + getResources());/**/
+    }
+
+    public String getMessage(){
+        return "Mensaje de funcion";
     }
 }

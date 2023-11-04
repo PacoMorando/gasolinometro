3e963cd3366b385efb50156b6b5a5059c490e25b
NYP_PC <paco.morando@gmail.com>
2023-11-03 20:03:24 -0600

    insert y selectAll funcionando, limpieza

--- app/src/main/java/sas/gasolinometro/RegistryController.java
+++ app/src/main/java/sas/gasolinometro/RegistryController.java
@@ -9,23 +9,22 @@ import java.util.ArrayList;
 public class RegistryController {
     protected float previousVehicleKms = 0.0f;
     private final ArrayList<ConsumptionRegister> consumptionRegistries;
-    private ConsumptionRegistryDAO consumptionRegistryDAO;
+    private final ConsumptionRegistryDAO consumptionRegistryDAO;
 
     public RegistryController(Context applicationContext) {
-        AppDatabase dataBase = Room.databaseBuilder(applicationContext, AppDatabase.class, "consumption-registry").build();
+        AppDatabase dataBase = Room.databaseBuilder(applicationContext, AppDatabase.class, "consumption-registry").allowMainThreadQueries().build();
         this.consumptionRegistryDAO = dataBase.consumptionRegistryDAO();
-        this.consumptionRegistries = new ArrayList<>();
-        //this.setMockConsumptionRegistries();
+        this.consumptionRegistries = (ArrayList<ConsumptionRegister>) this.consumptionRegistryDAO.getAll();
     }
 
     public void createRegister(float gasLoaded, float currentVehicleKms) {
-        //this.consumptionRegistries.add(new ConsumptionRegister(gasLoaded, currentVehicleKms, this.lastVehicleKms));
-        //this.consumptionRegistryDAO.insert(new ConsumptionRegister(gasLoaded, currentVehicleKms, this.lastVehicleKms));
-        this.consumptionRegistries.add(new ConsumptionRegister.Builder()
+        ConsumptionRegister consumptionRegister = new ConsumptionRegister.Builder()
                 .setGasLoaded(gasLoaded)
                 .setCurrentVehicleKms(currentVehicleKms)
                 .setPreviousVehicleKms(this.previousVehicleKms)
-                .build());
+                .build();
+        this.consumptionRegistries.add(consumptionRegister);
+        this.consumptionRegistryDAO.insert(consumptionRegister);
         this.previousVehicleKms = currentVehicleKms;
     }
 
@@ -33,16 +32,4 @@ public class RegistryController {
         return consumptionRegistries;
     }
 
-    private void setMockConsumptionRegistries() {
-      /*  this.consumptionRegistries.add(new ConsumptionRegister(9.097f, 250.2f, lastVehicleKms));
-        this.lastVehicleKms = 250.2f;
-        this.consumptionRegistries.add(new ConsumptionRegister(8.087f, 501.2f, lastVehicleKms));
-        this.lastVehicleKms = 501.2f;
-        this.consumptionRegistries.add(new ConsumptionRegister(8.187f, 788.2f, lastVehicleKms));
-        this.lastVehicleKms = 788.2f;
-        this.consumptionRegistries.add(new ConsumptionRegister(8.287f, 1050.2f, lastVehicleKms));
-        this.lastVehicleKms = 1050.2f;
-        this.consumptionRegistries.add(new ConsumptionRegister(8.387f, 1305.2f, lastVehicleKms));
-        this.lastVehicleKms = 1305.2f;*/
-    }
 }
